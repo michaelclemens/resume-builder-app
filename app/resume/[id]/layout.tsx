7603e76840324ed6839e2baa1bc8ba582018ec15
michaelclemens <michaelclemens8@hotmail.com>
2024-07-22 17:14:06 +0100

    Adding smooth animations to resume preview when reordering lists

--- app/resume/[id]/layout.tsx
+++ app/resume/[id]/layout.tsx
@@ -1,6 +1,6 @@
 import { getResume } from "@/lib/client/resume";
+import Link from "next/link";
 import { notFound } from "next/navigation";
-import { Suspense } from "react";
  
 export default async function Layout(
   { params: { id }, children, personal, employment, education, skill, strength }: 
@@ -10,19 +10,26 @@ export default async function Layout(
   if (!resume) notFound()
 
   return (
-    <div className="w-full h-screen flex flex-row overflow-hidden">
-      <div className="w-3/4">
-          <div className="h-full p-3 flex flex-col overflow-y-auto">
-            {personal}
-            {employment}
-            {education}
-            {skill}
-            {strength}
-          </div>
+    <div className="flex flex-col h-screen overflow-hidden">
+      <div className="w-full flex flex-row flex-grow overflow-hidden">
+        <div className="w-3/4 flex-shrink flex-grow-0">
+            <div className="flex flex-col overflow-y-auto h-full p-3">
+              {personal}
+              {employment}
+              {education}
+              {skill}
+              {strength}
+            </div>
+        </div>
+        <main role="main" className="w-full h-full flex-grow overflow-y-auto bg-gray-600">
+          {children}
+        </main>
       </div>
-      <main role="main" className="w-full h-full overflow-y-auto bg-gray-600">
-        {children}
-      </main>
+      <footer className="w-full px-6 py-5 bg-white border-t divide-slate-400/20 flex items-center justify-between">
+        <span className="text-sm text-gray-700 text-center font-semibold">
+          <Link href="/">Back</Link>
+        </span>
+      </footer>
     </div>
   )
 }
--- containers/ResumePreview.tsx
+++ containers/ResumePreview.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { selectIsLoadingResume } from "@/lib/redux/reducers";
 import { selectEducationList } from "@/lib/redux/reducers/education";
 import { selectEmploymentList } from "@/lib/redux/reducers/employment";
 import { selectPersonalDetails } from "@/lib/redux/reducers/personal";
@@ -11,8 +10,10 @@ import { sortByOrder } from "@/util/sort";
 import { RenderHtml } from "@/components/util";
 import Loading from "@/app/loading";
 import { ExpandableWrapper } from "@/components/util";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 export default function ResumePreview() {
+    const [animationParent] = useAutoAnimate();
     const personal = useAppSelector(selectPersonalDetails);
     const employments = useAppSelector(selectEmploymentList);
     const educations = useAppSelector(selectEducationList);
@@ -22,62 +23,71 @@ export default function ResumePreview() {
     const loading = [employments, educations, skills, strengths].some(value => !value);
 
     return (
-        <div className="bg-white mx-20 my-8 p-8">
+        <div className="bg-white mx-20 my-8 p-8 min-h-screen">
             <ExpandableWrapper open={!loading} initialMaxHeight="max-h-24">
                 {loading ? <Loading /> : (
                     <div className="flex gap-x-10">
                         <div className="w-2/6">
-                            <h3 className="text-xl pb-1 border-b font-semibold">Details</h3>
-                            <ul className="mt-2 mb-10">
-                                {(personal?.city || personal?.country) && (
-                                    <li className="px-2 mt-1">
-                                        <strong>Address</strong>
-                                        {personal?.city && <span className="block">{personal?.city}</span>}
-                                        {personal?.country && <span className="block">{personal?.country}</span>}
-                                    </li>
-                                )}
-                                {personal?.phone && (
-                                    <li className="px-2 mt-1">
-                                        <strong>Phone</strong>
-                                        <span className="block">{personal?.phone}</span>
-                                    </li>
-                                )}
-                                {personal?.email && (
-                                    <li className="px-2 mt-1">
-                                        <strong>Email</strong>
-                                        <span className="block">{personal?.email}</span>
-                                    </li>
-                                )}
-                            </ul>
-                            
-                            {skills && skills.length > 0 && (
+                            {personal && (
                                 <>
-                                    <h3 className="text-xl pb-1 border-b font-semibold">Experience</h3>
+                                    <h3 className="text-xl pb-1 border-b font-semibold">Details</h3>
                                     <ul className="mt-2 mb-10">
-                                        {[...skills].sort(sortByOrder).map(skill => <li key={skill.id} className="px-2 mt-1">{skill.name}</li>)}
+                                        {(personal?.city || personal?.country) && (
+                                            <li className="px-2 mt-1">
+                                                <strong>Address</strong>
+                                                {personal?.city && <span className="block">{personal?.city}</span>}
+                                                {personal?.country && <span className="block">{personal?.country}</span>}
+                                            </li>
+                                        )}
+                                        {personal?.phone && (
+                                            <li className="px-2 mt-1">
+                                                <strong>Phone</strong>
+                                                <span className="block">{personal?.phone}</span>
+                                            </li>
+                                        )}
+                                        {personal?.email && (
+                                            <li className="px-2 mt-1">
+                                                <strong>Email</strong>
+                                                <span className="block">{personal?.email}</span>
+                                            </li>
+                                        )}
                                     </ul>
-                                </>
-                            )}
+                                    
+                                    {skills && skills.length > 0 && (
+                                        <>
+                                            <h3 className="text-xl pb-1 border-b font-semibold">Experience</h3>
+                                            <ul ref={animationParent} className="mt-2 mb-10">
+                                                {[...skills].sort(sortByOrder).map(skill => <li key={skill.id} className="px-2 mt-1">{skill.name}</li>)}
+                                            </ul>
+                                        </>
+                                    )}
 
-                            {strengths && strengths.length > 0 && (
-                                <>
-                                    <h3 className="text-xl pb-1 border-b font-semibold">Strengths</h3>
-                                    <ul className="mt-2 mb-10">
-                                        {[...strengths].sort(sortByOrder).map(strength => <li key={strength.id} className="px-2 mt-1">{strength.name}</li>)}
-                                    </ul>
+                                    {strengths && strengths.length > 0 && (
+                                        <>
+                                            <h3 className="text-xl pb-1 border-b font-semibold">Strengths</h3>
+                                            <ul ref={animationParent} className="mt-2 mb-10">
+                                                {[...strengths].sort(sortByOrder).map(strength => <li key={strength.id} className="px-2 mt-1">{strength.name}</li>)}
+                                            </ul>
+                                        </>
+                                    )}
                                 </>
                             )}
+                            
                         </div>
                         <div className="w-4/6">
-                            <h2 className="text-2xl pb-1 border-b font-semibold">Profile</h2>
-                            <div className="mt-4">
-                                <RenderHtml html={personal?.summary ?? ''} />
-                            </div>
-                            
+                            {personal && (
+                                <>
+                                    <h2 className="text-2xl pb-1 border-b font-semibold">Profile</h2>
+                                    <div className="mt-4">
+                                        <RenderHtml html={personal?.summary ?? ''} />
+                                    </div>
+                                </>
+                            )}
+                        
                             {employments && employments.length > 0 && (
                                 <>
                                     <h2 className="text-2xl mt-6 pb-1 border-b font-semibold">Employment History</h2>
-                                    <ul className="mt-2">
+                                    <ul ref={animationParent} className="mt-2">
                                         {[...employments].sort(sortByOrder).map(employment => (
                                             <li key={employment.id} className="pt-2">
                                                 <p className="flex justify-between text-sm">
@@ -86,7 +96,7 @@ export default function ResumePreview() {
                                                 </p>
                                                 
                                                 {(employment.history && employment.history.length) && (
-                                                    <ul className="mt-2">
+                                                    <ul ref={animationParent} className="mt-2">
                                                         {[...employment.history].sort(sortByOrder).map(history => (
                                                             <li key={history.id} className="pt-2">
                                                                 <p className="flex justify-between text-base">{history.title}</p>
@@ -108,7 +118,7 @@ export default function ResumePreview() {
                             {educations && educations.length > 0 && (
                                 <>
                                     <h2 className="text-2xl mt-6 pb-1 border-b font-semibold">Education</h2>
-                                    <ul className="mt-2">
+                                    <ul ref={animationParent} className="mt-2">
                                         {[...educations].sort(sortByOrder).map(education => (
                                             <li key={education.id} className="pt-2">
                                                 <p className="flex justify-between text-sm">
