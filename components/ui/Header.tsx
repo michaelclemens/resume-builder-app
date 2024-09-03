32f1009055d10055b5d52846d8250ccaf734f97c
michaelclemens <michaelclemens8@hotmail.com>
2024-09-03 21:33:19 +0100

    Improving Home button

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -5,7 +5,6 @@ export default function Header() {
   return (
     <header className="z-10 flex w-full items-center justify-between border-b border-b-slate-300/60 px-6 dark:border-b-slate-400/20">
       <HomeButton />
-      <div className="my-2 ml-10 p-2 text-5xl font-bold tracking-wide text-sky-500/90 dark:text-violet-700/90">Resume Builder</div>
       <ThemeToggle />
     </header>
   )
--- components/ui/HomeButton.tsx
+++ components/ui/HomeButton.tsx
@@ -1,12 +1,19 @@
 import Link from 'next/link'
-import { FaHome } from 'react-icons/fa'
+import { Alexandria } from 'next/font/google'
+import { RxResume } from 'react-icons/rx'
+
+const alexandria = Alexandria({ subsets: ['latin'], display: 'swap' })
 
 export default function HomeButton() {
   return (
-    <span className="transition-color text-center text-sm text-sky-500/90 duration-500 hover:text-sky-400 dark:text-violet-700/90 dark:hover:text-violet-600">
-      <Link href="/">
-        <FaHome title="Home" size="2.25em" />
-      </Link>
-    </span>
+    <Link href="/" title="Home">
+      <div className={`${alexandria.className} flex flex-row items-center py-2`}>
+        <RxResume className="text-green-700 dark:text-indigo-500" size="2.5rem" />
+        <span className="mx-2 h-10 w-1 rounded-md bg-green-700 dark:bg-indigo-500"></span>
+        <span className="bg-gradient-to-r from-green-700 via-teal-500 via-30% to-sky-400 bg-clip-text text-[2.5rem] font-semibold text-transparent dark:from-indigo-500 dark:via-purple-500 dark:via-30% dark:to-pink-500">
+          Resume Builder
+        </span>
+      </div>
+    </Link>
   )
 }
