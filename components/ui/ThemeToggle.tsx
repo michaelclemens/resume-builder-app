a147f42a4c4df324ab38025be476ef8bff77f904
michaelclemens <michaelclemens8@hotmail.com>
2025-02-22 00:07:49 +0000

    Fixing breaking changes since tailwind 4 update

--- components/ui/ThemeToggle.tsx
+++ components/ui/ThemeToggle.tsx
@@ -13,8 +13,10 @@ export default function ThemeToggle() {
       <label className="group relative z-20 flex cursor-pointer items-center justify-between p-2">
         <input type="checkbox" className="sr-only" onChange={() => toggleMode(!darkMode)} />
         <span
-          className={`flex h-8 w-16 rounded-full bg-gradient-to-r from-cyan-200 to-cyan-600 p-1 ring-1 ring-slate-400/20 after:h-6 after:w-6 after:rounded-full after:bg-white after:ring-1 after:ring-slate-400/20 after:duration-300 group-hover:after:translate-x-1 dark:from-black dark:to-slate-600 after:dark:translate-x-8 after:dark:bg-slate-900 group-hover:after:dark:translate-x-7`}
-        ></span>
+          className={`flex h-8 w-16 rounded-full bg-gradient-to-r from-cyan-200 to-cyan-600 p-1 ring-1 ring-slate-400/20 dark:from-black dark:to-slate-600`}
+        >
+          <span className="h-6 w-6 rounded-full bg-white ring-1 ring-slate-400/20 duration-300 group-hover:translate-x-1 dark:translate-x-8 dark:bg-slate-900 group-hover:dark:translate-x-7" />
+        </span>
         <span
           title={darkModeTitle}
           className="absolute translate-x-10 text-slate-500 opacity-0 duration-300 ease-in-out dark:opacity-100 group-hover:dark:translate-x-9"
